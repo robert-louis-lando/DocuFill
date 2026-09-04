7c078a494667c06b5a35e2ad93d3e82b3d9bd7c5
Robert Louis Lando <robertlouis_lando@outlook.com>
2026-09-04 19:23:08 -0400

    setup + base pages

---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<template>
+  <section>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page.</p>
+    </div>
+  </section>
+</template>
---
+++ src/views/NotFound/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="not-found">
+    <div class="not-found__content">
+      <h1>404</h1>
+      <p>Page not found.</p>
+      <router-link to="/">Go back home</router-link>
+    </div>
+  </section>
+</template>
